caa078922d9b378106e8d4accffccdebb32ee3fa
Mike Luz <onlymikeluz@gmail.com>
2017-05-18 19:23:17 -0400

    deploy

--- server/start.js
+++ server/start.js
@@ -4,6 +4,6 @@ var Promise = require('bluebird');
 
 server.on('request', require('../app'));
 
-server.listen(3001, function() {
+server.listen(process.env.PORT || 3001, function() {
 	console.log('Server is listening on port 3001!');
 });
